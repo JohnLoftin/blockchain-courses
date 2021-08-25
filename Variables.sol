6207cacc1019c203e940aee337c1499bf30695f9
JohnLoftin <jgloftin@gmail.com>
2021-08-25 10:57:50 -0400

    added more variable examples

--- Variables.sol
+++ Variables.sol
@@ -12,4 +12,30 @@ contract WorkingWithVariables {
     function setMyBool(bool _myBool) public {
         myBool = _myBool;
     }
+
+    uint8 public myUint8;
+
+    function incrementUint() public {
+        myUint8++;
+    }
+
+    function decrementUint() public {
+        myUint8--;
+    }
+
+    address public myAddress;
+
+    function setAddress(address _address) public {
+        myAddress = _address;
+    }
+
+    function getBalanceOfAddress() public view returns (uint256) {
+        return myAddress.balance;
+    }
+
+    string public myString;
+
+    function setMyString(string memory _myString) public {
+        myString = _myString;
+    }
 }
