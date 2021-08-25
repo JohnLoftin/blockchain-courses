c8d726a14cb8e07752d1c88edbd0fd8f2a620334
JohnLoftin <jgloftin@gmail.com>
2021-08-25 13:57:32 -0400

    removed unneeded variable

--- SendMoneyExample.sol
+++ SendMoneyExample.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 contract SendMoneyExample {
     uint256 public balanceRecieved;
-    address public owner;
 
     function receiveMoney() public payable {
         balanceRecieved += msg.value;
