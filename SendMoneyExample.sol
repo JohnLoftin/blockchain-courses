804287ea1350408ef011f87a86ad457d75a7ad64
JohnLoftin <jgloftin@gmail.com>
2021-08-25 15:30:46 -0400

    added timelock to sendmoneyexample

--- SendMoneyExample.sol
+++ SendMoneyExample.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.0;
 
 contract SendMoneyExample {
     uint256 public balanceRecieved;
+    uint256 public lockedUntil;
 
     function receiveMoney() public payable {
         balanceRecieved += msg.value;
+        lockedUntil = block.timestamp + 1 minutes;
     }
 
     function getBalance() public view returns (uint256) {
@@ -13,11 +15,15 @@ contract SendMoneyExample {
     }
 
     function withdrawMoney() public {
-        address payable to = payable(msg.sender);
-        to.transfer(getBalance());
+        if (lockedUntil < block.timestamp) {
+            address payable to = payable(msg.sender);
+            to.transfer(getBalance());
+        }
     }
 
     function withdrawMoneyTo(address payable _to) public {
-        _to.transfer(this.getBalance());
+        if (lockedUntil < block.timestamp) {
+            _to.transfer(getBalance());
+        }
     }
 }
